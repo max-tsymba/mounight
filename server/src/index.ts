2105581604611ff63b0469039c0409ec35802cb2
max-tsymba <young.bezero@gmail.com>
2021-12-06 16:03:09 +0200

    index base settings

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 6000;
+const app: any = express();
+const PORT: number = +process.env.PORT || 6000;
+const DB_URL: string = process.env.DB_URL;
 
-app.listen(PORT, () => console.log(`Worked on ${PORT}`));
+const start: any = async () => {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => console.log(`Server working on ${PORT}`));
+  } catch (e: any) {
+    console.log(e);
+  }
+};
+
+start();
