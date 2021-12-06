e510ea3aad70fa42f0b91bdb44838a13603a08ff
max-tsymba <young.bezero@gmail.com>
2021-12-06 22:42:30 +0200

    delete dist

--- server/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-var express_1 = __importDefault(require("express"));
-var app = (0, express_1["default"])();
-var PORT = 5000;
-app.listen(PORT, function () { return console.log("Worked!"); });
