c267625772a2f0d34764a1f0c4813e4bec6a9abe
max-tsymba <young.bezero@gmail.com>
2021-12-13 15:35:56 +0200

    auth service

---
+++ client/src/services/AuthService.ts
@@ -0,0 +1,28 @@
+import $api from '@/API';
+import { IAuthResponse } from '@/models/response/AuthResponse';
+import { AxiosResponse } from 'axios';
+
+export default class AuthService {
+  static async login(
+    email: string,
+    password: string,
+  ): Promise<AxiosResponse<IAuthResponse>> {
+    return $api.post<IAuthResponse>('/login', { email, password });
+  }
+
+  static async registration(
+    username: string,
+    email: string,
+    password: string,
+  ): Promise<AxiosResponse<IAuthResponse>> {
+    return $api.post<IAuthResponse>('/registration', {
+      username,
+      email,
+      password,
+    });
+  }
+
+  static async logout(): Promise<void> {
+    return $api.post('/logout');
+  }
+}
