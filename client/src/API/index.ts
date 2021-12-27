f9524bba3d43bc51957c0da88ad279b1dcf4af44
max-tsymba <young.bezero@gmail.com>
2021-12-27 05:53:21 -0800

    new api

--- client/src/API/index.ts
+++ client/src/API/index.ts
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import { IAuthResponse } from '@/models/response/AuthResponse';
+import axios, { AxiosResponse } from 'axios';
 
 export const API_URL = `http://localhost:5000/api`;
 
@@ -12,5 +13,35 @@ $api.interceptors.request.use((config: any) => {
   config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
   return config;
 });
+
+$api.interceptors.response.use(
+  (config: any) => {
+    return config;
+  },
+  async (error: any) => {
+    const originalRequest = error.config;
+    if (
+      error.response.status == 401 &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      originalRequest._isRetry = true;
+      try {
+        const response: AxiosResponse<IAuthResponse, any> =
+          await axios.get<IAuthResponse>(`${API_URL}/refresh`, {
+            withCredentials: true,
+          });
+        localStorage.setItem('token', response.data.accessToken);
+        return $api.request(originalRequest);
+      } catch (e: any) {
+        console.log('not autorization');
+      }
+    }
+    if (error.response.status == 400) {
+      console.log('400');
+    }
+    throw error;
+  },
+);
 
 export default $api;
